29d2da398a089ae8696b49f85ad0af3f71db8d3d
Andrés Ignacio Torres <andresitorresm@gmail.com>
2023-09-06 14:40:08 -0700

    feat: implement connection base class + tests

---
+++ hermes/connections/base.py
@@ -0,0 +1,70 @@
+"""
+Module to implement the base class and enums for custom
+connections to social media networks, chat services, etc.
+"""
+
+from enum import Enum
+
+
+class ConnectionMode(Enum):
+    """
+    Enum to represent connection modes
+    """
+
+    READ = 1
+    WRITE = 2
+
+
+class Connection:
+    """
+    Base class to represent a connection to a social media network,
+    chat service, etc.
+
+    Subclasses should implement the _fetch and _post methods, which will
+    be called depending on the modes each connection is set up with.
+    """
+
+    name: str
+    modes: list[ConnectionMode]
+
+    def __init__(self, name: str, modes: list[ConnectionMode]) -> None:
+        """
+        Initializes the connection with a name and a list of modes
+        """
+
+        self.name = name
+        self.modes = modes
+
+    def read(self) -> list[str]:
+        """
+        Read new messages from this connection
+        """
+
+        if ConnectionMode.READ not in self.modes:
+            return []
+
+        return self._fetch()
+
+    def write(self, messages: list[str]) -> None:
+        """
+        Write messages to this connection
+        """
+
+        if ConnectionMode.WRITE not in self.modes:
+            return
+
+        self._post(messages)
+
+    def _fetch(self) -> list[str]:
+        """
+        Fetch messages from this connection
+        """
+
+        raise NotImplementedError(f"Fetch not implemented for connection {self.name}")
+
+    def _post(self, messages: list[str]) -> None:
+        """
+        Post messages to this connection
+        """
+
+        raise NotImplementedError(f"Post not implemented for connection {self.name}")
---
+++ tests/test_connections/test_base.py
@@ -0,0 +1,48 @@
+"""
+Module for adding unit tests for the base Connection class
+and related code.
+"""
+
+import pytest
+
+from hermes.connections.base import Connection, ConnectionMode
+
+
+def test_connection() -> None:
+    """
+    Tests the base implementation of the Connection class,
+    including safeguards against using modes that haven't
+    been implemented.
+    """
+
+    # Read only connection base
+    connection_1 = Connection("Read Only", [ConnectionMode.READ])
+    assert connection_1.name == "Read Only"
+    assert connection_1.modes == [ConnectionMode.READ]
+
+    # This shouldn't raise any exceptions
+    connection_1.write(["test message"])
+
+    with pytest.raises(NotImplementedError):
+        connection_1.read()
+
+    # Write only connection base
+    connection_2 = Connection("Write Only", [ConnectionMode.WRITE])
+    assert connection_2.name == "Write Only"
+    assert connection_2.modes == [ConnectionMode.WRITE]
+
+    assert connection_2.read() == []
+
+    with pytest.raises(NotImplementedError):
+        connection_2.write(["test message"])
+
+    # Read/Write connection base
+    connection_3 = Connection("Read/Write", [ConnectionMode.READ, ConnectionMode.WRITE])
+    assert connection_3.name == "Read/Write"
+    assert connection_3.modes == [ConnectionMode.READ, ConnectionMode.WRITE]
+
+    with pytest.raises(NotImplementedError):
+        connection_1.read()
+
+    with pytest.raises(NotImplementedError):
+        connection_2.write(["test message"])
