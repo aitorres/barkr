2b1e746423d134532abb878a67acc2dc992d63c6
Andrés Ignacio Torres <andresitorresm@gmail.com>
2025-01-26 13:52:55 -0800

    chore: clean logging statements from barkr

--- barkr/__init__.py
+++ barkr/__init__.py
@@ -11,6 +11,10 @@ logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
 
+# Do not stream httpx logs on this stream handler
+httpx_logger = logging.getLogger("httpx")
+httpx_logger.propagate = False
+
 formatter = logging.Formatter(
     "[%(asctime)s %(levelname)s %(thread)d %(name)s] %(message)s"
 )
